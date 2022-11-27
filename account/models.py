dc01c59953a8ae6cd6e06e60a06f3f826d186c61
OMAZ-Afreet <q.zak003@gmail.com>
2022-11-28 03:29:02 +0400

    working on account

---
+++ account/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = []
+    
+    phone_number = PhoneNumberField(blank=True)
